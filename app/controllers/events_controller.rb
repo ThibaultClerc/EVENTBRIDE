38ddd925b19731e629a58565c752af9bc3105394
ThibaultClerc <thibault.clerc2@gmail.com>
2020-08-11 16:47:44 +0200

    view users and index ok

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,19 @@
 class EventsController < ApplicationController
   def index
+    @events = Event.all
+  end
+
+  def new
+  end
+
+  def show
+    @event = Event.find(params[:id])
+    @admin = Event.find(params[:id]).admin
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:start_date, :duration, :title, :description, :price, :location, :admin_id)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  
-  def show
-  end
 
+  def show
+    @user = User.find(params[:id])
+    @events = @user.events
+  end
+  
 end
