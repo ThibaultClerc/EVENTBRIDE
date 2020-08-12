1b549fa6f4b2aa875ec0787c2c50a2b354463627
ThibaultClerc <thibault.clerc2@gmail.com>
2020-08-12 19:47:57 +0200

    changed charges to attendances

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,9 +1,30 @@
 class AttendancesController < ApplicationController
 
   def new
+    @attendance = Attendance.new
+    @event = Event.find(params)
   end
 
   def create
+    # Amount in cents
+    @amount = 500
+  
+    customer = Stripe::Customer.create({
+      email: params[:stripeEmail],
+      source: params[:stripeToken],
+    })
+  
+    charge = Stripe::Charge.create({
+      customer: customer.id,
+      amount: @amount,
+      description: "Paiement de foobarlol",
+      currency: 'eur',
+    })
+  
+    rescue Stripe::CardError => e
+      flash[:error] = e.message
+      redirect_to new_attendance_path
+    
   end
 
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   resources :users, only: [:show]
   resources :events
   resources :attendances
-  resources :charges
+  # resources :charges
 end
