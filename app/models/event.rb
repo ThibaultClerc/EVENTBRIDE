107c262dbe023e45a79c467f0d3c4f0374b9b81d
ThibaultClerc <thibault.clerc2@gmail.com>
2020-08-19 17:22:54 +0200

    partials ok

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,6 +3,18 @@ class Event < ApplicationRecord
   has_many :participants, foreign_key:'participant_id', class_name: "User", through: :attendances
   belongs_to :admin, class_name: "User"
 
+  validates :title,
+    presence: true
+  
+  validates :price,
+    presence: true
+
+  validates :desccription,
+    presence: true
+  
+  validates :location,
+    presence: true
+
   # def is_attended_by?(user)
   #   self.participants.include?(user.id)
   # end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,23 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   
   after_create :welcome_send
   # after_update :reset_password_send
 
   has_many :attendances, foreign_key:'participant_id', class_name: "Attendance"
   has_many :events, foreign_key:'admin_id', class_name: "Event"
-
   has_one_attached :avatar
-  
+
+  validates :first_name,
+    presence: true
+
+  validates :last_name,
+    presence: true
+
+  validates :password,
+    presence: true
+
   def welcome_send
     UserMailer.welcome_email(self).deliver_now
   end
